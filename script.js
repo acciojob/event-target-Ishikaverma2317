3249606efce434deb0de93a4401eb0ead1f3bcc8
Ishika Verma <ishikaverma2317@gmail.com>
2025-08-28 10:41:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 class EventTarget {
     constructor() {
-      this.listeners = new Map();
+        // har event ke liye listeners store karenge
+        this.listeners = {};
     }
-  
+
     addEventListener(event, callback) {
-      
+        if (!this.listeners[event]) {
+            this.listeners[event] = [];
+        }
+        // duplicate listener na add ho
+        if (!this.listeners[event].includes(callback)) {
+            this.listeners[event].push(callback);
+        }
     }
-  
+
     removeEventListener(event, callback) {
-      
+        if (this.listeners[event]) {
+            this.listeners[event] = this.listeners[event].filter(fn => fn !== callback);
+        }
     }
-  
+
     dispatchEvent(event) {
-    
+        if (this.listeners[event]) {
+            this.listeners[event].forEach(fn => fn());
+        }
     }
-    
-  }
-  
+}
